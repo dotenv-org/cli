f6aad533c67f902eebfaa4cd8f3eb747527e7ec2
motdotla <mot@mot.la>
2021-05-10 11:08:00 -0700

    Clean up pull

--- src/commands/push.ts
+++ src/commands/push.ts
@@ -1,4 +1,6 @@
 import {Command} from '@oclif/command'
+import {AppendToGitignoreService} from '../services/append-to-gitignore-service'
+import {WriteEnvsService} from '../services/write-envs-service'
 import {PushService} from '../services/push-service'
 
 export default class Push extends Command {
