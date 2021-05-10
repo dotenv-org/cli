e62c8937e203542aaf7727e3661e2f818bcc6b56
motdotla <mot@mot.la>
2021-05-10 11:01:23 -0700

    Clean up push

--- src/commands/push.ts
+++ src/commands/push.ts
@@ -1,54 +1,17 @@
-import {Command, flags} from '@oclif/command'
-import {vars} from '../vars'
+import {Command} from '@oclif/command'
+import {PushService} from '../services/push-service'
 
 export default class Push extends Command {
   static description = 'push .env'
 
-  static flags = {}
-  static args = []
-
   async run() {
-    const {args, flags} = this.parse(Push)
-
     // 1. create gitignore
     new AppendToGitignoreService().run()
 
     // 2. create envs
     new WriteEnvsService().run()
 
-    const fs = require('fs')
-    const axios = require('axios')
-    const dotenv = require('dotenv')
-    const signale = require('signale')
-
-    signale.wait('pushing…')
-
-    const envProject = dotenv.config({ path: '.env.project' })
-    const envMe = dotenv.config({ path: '.env.me' })
-
-    const url = vars.apiUrl + '/v1/push'
-    const data = {
-      'projectUid': envProject.parsed['DOTENV_PROJECT'],
-      'meUid': envMe.parsed['DOTENV_ME'],
-      'dotenv': fs.readFileSync('.env', 'UTF-8')
-    }
-    const options = {
-      method: 'POST',
-      headers: { 'content-type': 'application/json' },
-      data: data,
-      url
-    }
-    axios(options)
-    .then(function (response) {
-      signale.success('pushed.')
-    })
-    .catch(function (error) {
-      if (error.response) {
-        signale.fatal(error.response.data)
-      } else {
-        signale.fatal(error)
-      }
-    })
-
+    // 3. push
+    new PushService().run()
   }
 }
---
+++ src/services/push-service.ts
@@ -0,0 +1,59 @@
+import * as fs from 'fs'
+import * as dotenv from 'dotenv'
+import * as axios from 'axios'
+import * as signale from 'signale'
+
+import {vars} from '../vars'
+
+class PushService {
+  async run() {
+    signale.wait('pushing…')
+
+    axios(this._pushOptions)
+    .then(_response => {
+      signale.success('pushed.')
+    })
+    .catch(function (error) {
+      if (error.response) {
+        signale.fatal(error.response.data)
+      } else {
+        signale.fatal(error)
+      }
+    })
+  }
+
+  get _pushOptions() {
+    const url = vars.apiUrl + '/v1/push'
+    const data = {
+      projectUid: this._DOTENV_PROJECT,
+      meUid: this._DOTENV_ME,
+      dotenv: fs.readFileSync('.env', 'UTF-8'),
+    }
+    const options = {
+      method: 'POST',
+      headers: {'content-type': 'application/json'},
+      data: data,
+      url,
+    }
+
+    return options
+  }
+
+  get _envMe() {
+    return dotenv.config({path: '.env.me'})
+  }
+
+  get _envProject() {
+    return dotenv.config({path: '.env.project'})
+  }
+
+  get _DOTENV_ME(): string {
+    return this._envMe.parsed.DOTENV_ME
+  }
+
+  get _DOTENV_PROJECT(): string {
+    return this._envProject.parsed.DOTENV_PROJECT
+  }
+}
+
+export {PushService}
