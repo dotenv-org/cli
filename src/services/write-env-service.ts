ec9cdd4c4f395d25f28c907211723861332487b3
Scott Motte <mot@mot.la>
2021-12-01 07:57:02 -0800

    Add write env service

---
+++ src/services/write-env-service.ts
@@ -0,0 +1,35 @@
+import * as fs from 'fs'
+import * as crypto from 'crypto'
+import * as signale from 'signale'
+
+interface WriteEnvServiceParams {
+  quiet?: boolean;
+}
+
+class WriteEnvService {
+  public quiet?: boolean;
+
+  constructor(params: WriteEnvServiceParams = {} as WriteEnvServiceParams) {
+    const {quiet = false} = params
+
+    this.quiet = quiet
+  }
+
+  run() {
+    const dotenvFile = '.env'
+    const dotenvData = `# added by dotenv - do not commit this file to code
+KEY=value`
+
+    // 1. write .env
+    if (fs.existsSync(dotenvFile)) {
+      if (!this.quiet) {
+        signale.success('Existing .env.')
+      }
+    } else {
+      fs.writeFileSync(dotenvFile, dotenvData)
+      signale.success('Created .env.')
+    }
+  }
+}
+
+export {WriteEnvService}
