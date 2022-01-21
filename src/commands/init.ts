1b7f39febc4ab041e0552d7ee3a7c99e07b7d47f
Scott Motte <mot@mot.la>
2022-01-20 22:46:31 -0800

    Change init to alias for `new`. Version 1.0.0

--- src/commands/init.ts
+++ src/commands/init.ts
@@ -1,32 +1,19 @@
 import {Command} from '@oclif/command'
 import {AppendToGitignoreService} from '../services/append-to-gitignore-service'
-import {WriteEnvsService} from '../services/write-envs-service'
-import {VerifyService} from '../services/verify-service'
 import {CheckLatestVersionService} from '../services/check-latest-version-service'
+import {OutputNewProjectLinkService} from '../services/output-new-project-link-service'
 
 export default class Init extends Command {
-  static description = 'DEPRECATED: initialize .env.me and .env.project'
-
-  static args = [
-    {name: 'organizationSlug'},
-  ]
+  static description = 'DEPRECATED. alias for `dotenv-cli new`'
 
   async run() {
-    const {args} = this.parse(Init)
-
     // 0. check latest version
     await new CheckLatestVersionService().run()
-
-    // Deprecation notice
-    console.log('DEPRECATION NOTICE: \'dotenv-cli init\' is deprecated. Going forward, run \'dotenv-cli new\' instead.')
 
     // 1. create gitignore
     new AppendToGitignoreService().run()
 
-    // 2. create envs
-    new WriteEnvsService().run()
-
-    // 3. verify
-    new VerifyService(args.organizationSlug).run()
+    // 2. Output new link
+    new OutputNewProjectLinkService().run()
   }
 }
