c09eee3b66ec2c9f255e01e8a7fdf809a9cb3954
Scott Motte <mot@mot.la>
2022-03-31 21:19:55 -0700

    Format notice

--- src/services/deprecation-service.ts
+++ src/services/deprecation-service.ts
@@ -2,9 +2,14 @@ class DeprecationService {
   async run(command) {
     console.log('********* Warning: Deprecated *********')
     console.log('')
-    console.log('This cli is deprecated. Please use dotenv-vault instead.')
+    console.log('        This cli is deprecated!')
+    console.log('    Please use dotenv-vault instead.')
     console.log('')
-    console.log(`    npx dotenv-vault ${command}`)
+    console.log('               It\'s easy.')
+    console.log('')
+    console.log(`       $ npx dotenv-vault ${command}`)
+    console.log('')
+    console.log('   Visit www.dotenv.org to learn more.')
     console.log('')
     console.log('***************************************')
   }
